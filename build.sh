b9cf6c5a2eba7bf911acdfc6e5ef88c534a4c4ed
Andrei <b1naryth1ef@gmail.com>
2016-08-16 00:00:27 -0500

    Remove build.sh

--- build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-dub build --combined --parallel ${@:1} &
-for dir in $(ls -d plugins/*/); do
-  pushd $dir
-  dub build --force --combined --parallel ${@:1} &
-  popd
-done
-wait
-echo "Finished building"
